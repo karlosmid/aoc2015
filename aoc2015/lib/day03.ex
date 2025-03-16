8330ac60bbe2ae222e6ffa11ed47ee2df9442169
karlo.smid <karlo.smid@tentamen.hr>
2025-03-16 21:03:28 +0100

    feat: day03 part2 done

--- aoc2015/lib/day03.ex
+++ aoc2015/lib/day03.ex
@@ -1,5 +1,5 @@
 defmodule Day03 do
-  def part1(input), do: part1(input, 0, 0, %{{0,0} => 1})
+  def part1(input), do: part1(input, 0, 0, %{{0, 0} => 1})
 
   defp part1([], _x, _y, visited_houses), do: Enum.count(visited_houses)
 
@@ -14,4 +14,91 @@ defmodule Day03 do
 
   defp part1([?> | rest], x, y, visited_houses),
     do: part1(rest, x + 1, y, Map.put(visited_houses, {x + 1, y}, 1))
+
+  def part2(input) do
+    part2(input, {0, 0}, {0, 0}, 0, %{{0, 0} => 1})
+  end
+
+  defp part2([], _santa_positon, _robo_positon, _input_position, visited_houses),
+    do: Enum.count(visited_houses)
+
+  defp part2([?^ | rest], {santa_x, santa_y}, {robo_x, robo_y}, input_position, visited_houses) do
+    if rem(input_position, 2) == 0 do
+      part2(
+        rest,
+        {santa_x, santa_y + 1},
+        {robo_x, robo_y},
+        input_position + 1,
+        Map.put(visited_houses, {santa_x, santa_y + 1}, 1)
+      )
+    else
+      part2(
+        rest,
+        {santa_x, santa_y},
+        {robo_x, robo_y + 1},
+        input_position + 1,
+        Map.put(visited_houses, {robo_x, robo_y + 1}, 1)
+      )
+    end
+  end
+
+  defp part2([?v | rest], {santa_x, santa_y}, {robo_x, robo_y}, input_position, visited_houses) do
+    if rem(input_position, 2) == 0 do
+      part2(
+        rest,
+        {santa_x, santa_y - 1},
+        {robo_x, robo_y},
+        input_position + 1,
+        Map.put(visited_houses, {santa_x, santa_y - 1}, 1)
+      )
+    else
+      part2(
+        rest,
+        {santa_x, santa_y},
+        {robo_x, robo_y - 1},
+        input_position + 1,
+        Map.put(visited_houses, {robo_x, robo_y - 1}, 1)
+      )
+    end
+  end
+
+  defp part2([?< | rest], {santa_x, santa_y}, {robo_x, robo_y}, input_position, visited_houses) do
+    if rem(input_position, 2) == 0 do
+      part2(
+        rest,
+        {santa_x - 1, santa_y},
+        {robo_x, robo_y},
+        input_position + 1,
+        Map.put(visited_houses, {santa_x - 1, santa_y}, 1)
+      )
+    else
+      part2(
+        rest,
+        {santa_x, santa_y},
+        {robo_x - 1, robo_y},
+        input_position + 1,
+        Map.put(visited_houses, {robo_x - 1, robo_y}, 1)
+      )
+    end
+  end
+
+  defp part2([?> | rest], {santa_x, santa_y}, {robo_x, robo_y}, input_position, visited_houses) do
+    if rem(input_position, 2) == 0 do
+      part2(
+        rest,
+        {santa_x + 1, santa_y},
+        {robo_x, robo_y},
+        input_position + 1,
+        Map.put(visited_houses, {santa_x + 1, santa_y}, 1)
+      )
+    else
+      part2(
+        rest,
+        {santa_x, santa_y},
+        {robo_x + 1, robo_y},
+        input_position + 1,
+        Map.put(visited_houses, {robo_x + 1, robo_y}, 1)
+      )
+    end
+  end
 end
--- aoc2015/test/day03_test.exs
+++ aoc2015/test/day03_test.exs
@@ -3,7 +3,7 @@ defmodule Day03Test do
 
   describe "part1" do
     test "empty input" do
-      assert Day03.part1(Data.string_as_charlist("")) == 1 
+      assert Day03.part1(Data.string_as_charlist("")) == 1
     end
 
     test ">" do
@@ -20,6 +20,28 @@ defmodule Day03Test do
 
     test "puzzle from file" do
       assert Day03.part1(Data.read_file_as_charlist("data/day03.txt")) == 2592
+    end
+  end
+
+  describe "part2" do
+    test "empty input" do
+      assert Day03.part2(Data.string_as_charlist("")) == 1
+    end
+
+    test "^v" do
+      assert Day03.part2(Data.string_as_charlist("^v")) == 3
+    end
+
+    test "^>v<" do
+      assert Day03.part2(Data.string_as_charlist("^>v<")) == 3
+    end
+
+    test "^v^v^v^v^v" do
+      assert Day03.part2(Data.string_as_charlist("^v^v^v^v^v")) == 11
+    end
+
+    test "puzzle from file" do
+      assert Day03.part2(Data.read_file_as_charlist("data/day03.txt")) == 2360
     end
   end
 end
