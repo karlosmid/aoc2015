4704e0ff68f3ebb18a410c69af61f05a9a91ecb2
karlo.smid <karlo.smid@tentamen.hr>
2025-04-01 11:02:54 +0100

    feat: day06 is done

---
+++ aoc2015/lib/day06.ex
@@ -0,0 +1,99 @@
+defmodule Day06 do
+  def part1(input) do
+    input
+    |> parse_input()
+    |> count_lights_on()
+  end
+
+  def part2(input) do
+    input
+    |> parse_input()
+    |> calculate_total_brightness()
+  end
+
+  defp parse_input(input) do
+    input
+    |> Enum.map(&parse_instruction/1)
+  end
+
+  def parse_instruction(instruction) do
+    case instruction do
+      "turn on " <> rest -> {:on, parse_coordinates(rest)}
+      "turn off " <> rest -> {:off, parse_coordinates(rest)}
+      "toggle " <> rest -> {:toggle, parse_coordinates(rest)}
+    end
+  end
+
+  defp parse_coordinates(coords) do
+    [start, "through", finish] = String.split(coords, " ")
+    [start_x, start_y] = String.split(start, ",") |> Enum.map(&String.to_integer/1)
+    [end_x, end_y] = String.split(finish, ",") |> Enum.map(&String.to_integer/1)
+    {{start_x, start_y}, {end_x, end_y}}
+  end
+
+  defp count_lights_on(instructions) do
+    instructions
+    |> Enum.reduce(%{}, fn instruction, grid ->
+      apply_instruction(grid, instruction)
+    end)
+    |> Map.values()
+    |> Enum.count(&(&1 == true))
+  end
+
+  defp calculate_total_brightness(instructions) do
+    instructions
+    |> Enum.reduce(%{}, fn instruction, grid ->
+      apply_brightness_instruction(grid, instruction)
+    end)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  defp apply_instruction(grid, {:on, {{start_x, start_y}, {end_x, end_y}}}) do
+    grid_on = %{}
+    for x <- start_x..end_x, y <- start_y..end_y do
+      Map.put(grid_on, {x, y}, true)
+    end
+    |> Enum.reduce(grid, &Map.merge(&2, &1))
+  end
+
+  defp apply_instruction(grid, {:off, {{start_x, start_y}, {end_x, end_y}}}) do
+    grid_off = %{}
+    for x <- start_x..end_x, y <- start_y..end_y do
+      Map.put(grid_off, {x, y}, false)
+    end
+    |> Enum.reduce(grid, &Map.merge(&2, &1))
+  end
+
+  defp apply_instruction(grid, {:toggle, {{start_x, start_y}, {end_x, end_y}}}) do
+    grid_toggle = %{}
+    for x <- start_x..end_x, y <- start_y..end_y do
+      Map.put(grid_toggle, {x, y}, !Map.get(grid, {x, y}, false))
+    end
+    |> Enum.reduce(grid, &Map.merge(&2, &1))
+  end
+
+  defp apply_brightness_instruction(grid, {:on, {{start_x, start_y}, {end_x, end_y}}}) do
+    grid_on = %{}
+    for x <- start_x..end_x, y <- start_y..end_y do
+      Map.put(grid_on, {x, y}, Map.get(grid, {x, y}, 0) + 1)
+    end
+    |> Enum.reduce(grid, &Map.merge(&2, &1))
+  end
+
+  defp apply_brightness_instruction(grid, {:off, {{start_x, start_y}, {end_x, end_y}}}) do
+    grid_off = %{}
+    for x <- start_x..end_x, y <- start_y..end_y do
+      Map.put(grid_off, {x, y}, max(0, Map.get(grid, {x, y}, 0) - 1))
+    end
+    |> Enum.reduce(grid, &Map.merge(&2, &1))
+  end
+
+  defp apply_brightness_instruction(grid, {:toggle, {{start_x, start_y}, {end_x, end_y}}}) do
+    grid_toggle = %{}
+    for x <- start_x..end_x, y <- start_y..end_y do
+      Map.put(grid_toggle, {x, y}, Map.get(grid, {x, y}, 0) + 2)
+    end
+    |> Enum.reduce(grid, &Map.merge(&2, &1))
+  end
+end 
---
+++ aoc2015/test/day06_test.exs
@@ -0,0 +1,86 @@
+defmodule Day06Test do
+  use ExUnit.Case
+
+  describe "part1/1" do
+    test "empty input returns 0" do
+      assert Day06.part1([]) == 0
+    end
+
+    test "turn on all lights" do
+      input = ["turn on 0,0 through 999,999"]
+      assert Day06.part1(input) == 1_000_000
+    end
+
+    test "toggle first row" do
+      input = ["toggle 0,0 through 999,0"]
+      assert Day06.part1(input) == 1000
+    end
+
+    test "turn off middle section" do
+      input = ["turn off 499,499 through 500,500"]
+      assert Day06.part1(input) == 0
+    end
+
+    test "multiple instructions" do
+      input = ["turn on 0,0 through 0,1", "toggle 0,0 through 0,1", "turn off 0,0 through 0,1"]
+      assert Day06.part1(input) == 0
+    end
+
+    test "puzzle input" do
+      input = Data.read_file_as_list_of_strings("data/day06.txt")
+      assert Day06.part1(input) == 400410
+    end
+  end
+
+  describe "part2/1" do
+    test "empty input returns 0" do
+      assert Day06.part2([]) == 0
+    end
+
+    test "turn on all lights" do
+      input = "turn on 0,0 through 999,999"
+      assert Day06.part2([input]) == 1_000_000
+    end
+
+    test "toggle first row" do
+      input = "toggle 0,0 through 999,0"
+      assert Day06.part2([input]) == 2000
+    end
+
+    test "turn off middle section" do
+      input = "turn off 499,499 through 500,500"
+      assert Day06.part2([input]) == 0
+    end
+
+    test "multiple instructions" do
+      input = [
+        "turn on 0,0 through 0,1",
+        "toggle 0,0 through 0,1",
+        "turn off 0,0 through 0,1"
+      ]
+      assert Day06.part2(input) == 4
+    end
+
+    test "puzzle input" do
+      input = Data.read_file_as_list_of_strings("data/day06.txt")
+      assert Day06.part2(input) == 15343601
+    end
+  end
+
+  describe "parse_instruction/1" do
+    test "parses turn on instruction" do
+      assert Day06.parse_instruction("turn on 0,0 through 999,999") == 
+        {:on, {{0, 0}, {999, 999}}}
+    end
+
+    test "parses turn off instruction" do
+      assert Day06.parse_instruction("turn off 499,499 through 500,500") == 
+        {:off, {{499, 499}, {500, 500}}}
+    end
+
+    test "parses toggle instruction" do
+      assert Day06.parse_instruction("toggle 0,0 through 999,0") == 
+        {:toggle, {{0, 0}, {999, 0}}}
+    end
+  end
+end 
